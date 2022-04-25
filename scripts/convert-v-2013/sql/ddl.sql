99ddc04a9d1ebf0b8a09653cb4edfa04f125b0a3
Keiichiro Nishimori <rendlire@gmail.com>
2022-04-26 01:58:09 +0900

    add export sql

--- scripts/convert-v-2013/sql/ddl.sql
+++ scripts/convert-v-2013/sql/ddl.sql
@@ -88,6 +88,12 @@ CREATE TABLE IF NOT EXISTS tag_2013 (
     tag_name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS tag_intedgrated (
+    code VARCHAR(255) NOT NULL,
+    tag_name VARCHAR(255) NOT NULL,
+    CONSTRAINT un1_tag_inte UNIQUE (code, tag_name)
+);
+
 CREATE INDEX i1_tag_2021 ON tag_2021 (code);
 CREATE INDEX i2_tag_2021 ON tag_2021 (tag_name);
 CREATE INDEX i1_tag_2018 ON tag_2018 (code);
@@ -96,3 +102,6 @@ CREATE INDEX i1_tag_2016 ON tag_2016 (code);
 CREATE INDEX i2_tag_2016 ON tag_2016 (tag_name);
 CREATE INDEX i1_tag_2013 ON tag_2013 (code);
 CREATE INDEX i2_tag_2013 ON tag_2013 (tag_name);
+
+CREATE INDEX I1_tag_intedgrated ON tag_intedgrated (code);
+CREATE INDEX I2_tag_intedgrated ON tag_intedgrated (tag_name);
--- scripts/convert-v-2013/sql/merge.sql
+++ scripts/convert-v-2013/sql/merge.sql
@@ -158,3 +158,22 @@ SELECT
 FROM
     tag_2013
 ON CONFLICT DO NOTHING;
+
+
+INSERT INTO tag_intedgrated (code, tag_name) SELECT code, tag_name FROM tag_2021 ON CONFLICT DO NOTHING;
+INSERT INTO tag_intedgrated (code, tag_name) SELECT code, tag_name FROM tag_2018 ON CONFLICT DO NOTHING;
+INSERT INTO tag_intedgrated (code, tag_name) SELECT code, tag_name FROM tag_2016 ON CONFLICT DO NOTHING;
+INSERT INTO tag_intedgrated (code, tag_name) SELECT code, tag_name FROM tag_2013 ON CONFLICT DO NOTHING;
+
+EXPLAIN
+INSERT INTO video_tag_relation (
+    video_id
+,   tag_id
+)
+SELECT
+    video.id
+,   tag.id
+FROM
+    tag_intedgrated
+INNER JOIN video ON video.code = tag_intedgrated.code
+INNER JOIN tag ON tag.name = tag_intedgrated.tag_name;
