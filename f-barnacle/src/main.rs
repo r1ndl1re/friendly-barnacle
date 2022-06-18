74d138391547589a7aa3336afd30d3f377a57446
Keiichiro Nishimori <rendlire@gmail.com>
2022-06-18 21:58:15 +0900

    add tracing crate

--- f-barnacle/src/main.rs
+++ f-barnacle/src/main.rs
@@ -1,7 +1,35 @@
 mod entity;
 
-use entity::{prelude::*, video};
-use sea_orm::{ColumnTrait, ConnectOptions, Database, EntityTrait, QueryFilter};
+use std::net::SocketAddr;
+
+use axum::extract::Query;
+use axum::{extract::Extension, routing::get, Router};
+use entity::prelude::*;
+use entity::{tag, video};
+use sea_orm::{
+    ColumnTrait, ConnectOptions, Database, DatabaseConnection, EntityTrait, QueryFilter,
+};
+
+async fn read_video(db: &DatabaseConnection, video_id: &str) -> Option<video::Model> {
+    let video = Video::find()
+        .filter(video::Column::Code.eq(video_id))
+        .one(db)
+        .await
+        .unwrap();
+    video
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn get_video(
+    Extension(db): Extension<DatabaseConnection>,
+    video_id: Query<&str>,
+) -> Option<video::Model> {
+    let video = read_video(&db, &video_id).await;
+    video
+}
 
 #[tokio::main]
 async fn main() {
@@ -13,11 +41,20 @@ async fn main() {
 
     let db = Database::connect(opt).await.unwrap();
 
-    let video = Video::find()
-        .filter(video::Column::Code.eq("aaaa"))
-        .one(&db)
-        .await
-        .unwrap();
+    let video_id = "aaa";
+    let video = read_video(&db, video_id).await;
 
     println!("{:?}", video);
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/video", get(get_video))
+        .layer(Extension(db));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing_subscriber::fmt::init();
+    tracing::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
